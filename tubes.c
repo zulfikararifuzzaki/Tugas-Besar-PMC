919a6268fe06bb7baf88c0f4d42af3819995135a
zulfikararifuzzaki <60221374+zulfikararifuzzaki@users.noreply.github.com>
2020-04-05 13:15:38 +0700

    Update tubes.c

--- tubes.c
+++ tubes.c
@@ -9,13 +9,22 @@ struct komponen{
 	int value;
 	struct komponen *kiri;
 	struct komponen *kanan;
-	};
+	} komponen;
 
 int main()
 {	//DEKLARASI VARIABEL
 	FILE *fptr;
 	char* fname = (char*)malloc(sizeof(char));
 	char data[50];
+	komponen resistor[5] = (komponen*)malloc(sizeof(komponen));
+	komponen kapasitor[5] = (komponen*)malloc(sizeof(komponen));
+	komponen temp = (komponen*)malloc(sizeof(komponen));
+	int inc_r = 0;
+	int inc_c = 0;
+	float r_eki = 0;
+	float c_eki = 0;
+	int inc1 = 0;
+	int inc2 = 0;
 	
 	//INPUT FILE
 	printf("Masukkan nama file external : ");
@@ -28,6 +37,26 @@ int main()
 		while(!feof(fptr))
 		{	fgets(data, sizeof(data), fptr);
 			//DIBAWAH INI AKAN DIIMPLEMETASIKAN FUNGSI UNTUK MEMECAH "data" menjadi perbagian
+		 	temp = pecah(data);
+		 	if(temp.jenis == 'R')
+			{
+				resistor[inc_r] = temp;
+				++inc_r;
+			}
+		 	else
+			{
+				kapasitor[inc_k] = temp;
+				++inc_k;
+			}
+		}
+		//SAMPAI DISINI SEMUA RESISTOR DAN KAPASITOR SUDAH MASUK ARRAY
+		//mencari R ekivalen
+		for (inc1 = 0; inc1<5; ++inc1)
+		{
+			for(inc2 = 5; inc2>5-inc1; --inc2)
+			{
+				if ()
+			}
 		}
 	}
   return 0;
@@ -43,13 +72,13 @@ komponen pecah(char* data)
  	while(data[i]!=NULL)
 	{	while(data[i] != delimiter)
 		{	temp[i] = data[i];
-			++i; }
+			++i; }\
+		temp[i+1] = '\0';
 	 	i = 0;
 	 	if (j == 0)	out->jenis = temp[0];
 	 	else if (j == 1)	out->value = atoi(temp);
 	 	else if (j == 2)	out->kiri = temp;
 	 	else	out->kanan = data;
+		++j;
 	}
- 	
-
 }
